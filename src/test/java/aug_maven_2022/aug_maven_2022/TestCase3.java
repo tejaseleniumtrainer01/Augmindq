042460b77776a0115477b3b030474a7bdf58d281
Owner <Owner@192.168.2.21>
2022-09-01 06:48:31 -0400

    uploading new test case

---
+++ src/test/java/aug_maven_2022/aug_maven_2022/TestCase3.java
@@ -0,0 +1,11 @@
+package aug_maven_2022.aug_maven_2022;
+
+public class TestCase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First code of Test case3");
+
+	}
+
+}
